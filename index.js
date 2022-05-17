1b4db0bbe5ba87f3706d9e6bc70553db88473b21
Alextonio-Winatian <101455767+Alextonio-Winatian@users.noreply.github.com>
2022-05-17 14:53:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const pool = mysql.createPool({
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_NAME,
-	socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+	socketPath: process.env.INSTANCE_CONNECTION_NAME,
 });
 
 app.post("/", async (req, res)=>{
